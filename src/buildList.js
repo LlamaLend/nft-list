630d65b9a33454111277a2a6f9eff00a27bd1f0b
shibacrypt <>
2022-12-04 21:20:23 +0800

    Cleanup

--- src/buildList.js
+++ src/buildList.js
@@ -1,18 +1,10 @@
 const { version } = require("../package.json");
 const mainnet = require("./tokens/mainnet.json");
-const ropsten = require("./tokens/ropsten.json");
-const rinkeby = require("./tokens/rinkeby.json");
-const goerli = require("./tokens/goerli.json");
-const kovan = require("./tokens/kovan.json");
-const polygon = require("./tokens/polygon.json");
-const mumbai = require("./tokens/mumbai.json");
-const optimism = require("./tokens/optimism.json");
-const celo = require("./tokens/celo.json");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
   return {
-    name: "Uniswap Labs Default",
+    name: "NFT Default",
     timestamp: new Date().toISOString(),
     version: {
       major: +parsed[0],
@@ -21,8 +13,8 @@ module.exports = function buildList() {
     },
     tags: {},
     logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
-    keywords: ["uniswap", "default"],
-    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...polygon, ...mumbai, ...optimism, ...celo]
+    keywords: ["nft", "default"],
+    tokens: [...mainnet]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
